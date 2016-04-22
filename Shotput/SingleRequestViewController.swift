ff2b453110a19aa2bcff7b747c01937c5bff8252
Matthew Lewis <matt@mplewis.com>
2016-04-22 16:37:38 -0500

    vc: custom header row from Eureka examples

--- Shotput/SingleRequestViewController.swift
+++ Shotput/SingleRequestViewController.swift
@@ -1,21 +1,21 @@
 import UIKit
 import Eureka
+import Alamofire
+import MapKit
 
 class SingleRequestViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.form
-            +++= DateRow("Date") {
-                $0.value = NSDate()
-                $0.title = "The clock says:"
+            +++= SegmentedRow<Alamofire.Method>("Method") {
+                $0.title = "Method"
+                $0.options = [.GET, .POST];
+                $0.value = .GET
             }
-            <<< CheckRow("Check") {
-                $0.title = "Check"
-                $0.value = true
-            }
-            <<< PhoneRow("Phone") {
-                $0.placeholder = "Phone"
+            <<< LocationRow() {
+                $0.title = "LocationRow"
+                $0.value = CLLocation(latitude: -34.91, longitude: -56.1646)
             }
     }
 
