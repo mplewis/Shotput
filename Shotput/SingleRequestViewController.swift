a87b969e7d85c0f9b8bd603fa3e3397c8a3fc8ec
Matthew Lewis <matt@mplewis.com>
2016-04-22 20:03:38 -0500

    vc: replace Eureka with XLForm

--- Shotput/SingleRequestViewController.swift
+++ Shotput/SingleRequestViewController.swift
@@ -1,22 +1,61 @@
 import UIKit
-import Eureka
+import XLForm
 import Alamofire
 import MapKit
 
-class SingleRequestViewController: FormViewController {
+class SingleRequestViewController: XLFormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.form
-            +++= SegmentedRow<Alamofire.Method>("Method") {
-                $0.title = "Method"
-                $0.options = [.GET, .POST];
-                $0.value = .GET
-            }
-            <<< LocationRow() {
-                $0.title = "LocationRow"
-                $0.value = CLLocation(latitude: -34.91, longitude: -56.1646)
-            }
+        
+        self.form = XLFormDescriptor(title: "Add Event")
+        var section: XLFormSectionDescriptor
+        var row: XLFormRowDescriptor
+        
+        section = XLFormSectionDescriptor.formSection()
+        
+        row = XLFormRowDescriptor(tag: "title", rowType: XLFormRowDescriptorTypeText)
+        row.cellConfigAtConfigure["textField.placeholder"] = "Title"
+        section.addFormRow(row)
+        
+        self.form.addFormSection(section)
+
+        /*
+        
+        XLFormDescriptor * form;
+        XLFormSectionDescriptor * section;
+        XLFormRowDescriptor * row;
+        
+        form = [XLFormDescriptor formDescriptorWithTitle:@"Add Event"];
+        
+        // First section
+        section = [XLFormSectionDescriptor formSection];
+        [form addFormSection:section];
+        
+        // Title
+        row = [XLFormRowDescriptor formRowDescriptorWithTag:@"title" rowType:XLFormRowDescriptorTypeText];
+        [row.cellConfigAtConfigure setObject:@"Title" forKey:@"textField.placeholder"];
+        [section addFormRow:row];
+        
+        // Location
+        row = [XLFormRowDescriptor formRowDescriptorWithTag:@"location" rowType:XLFormRowDescriptorTypeText];
+        [row.cellConfigAtConfigure setObject:@"Location" forKey:@"textField.placeholder"];
+        [section addFormRow:row];
+        
+        // Second Section
+        section = [XLFormSectionDescriptor formSection];
+        [form addFormSection:section];
+        
+        // All-day
+        row = [XLFormRowDescriptor formRowDescriptorWithTag:@"all-day" rowType:XLFormRowDescriptorTypeBooleanSwitch title:@"All-day"];
+        [section addFormRow:row];
+        
+        // Starts
+        row = [XLFormRowDescriptor formRowDescriptorWithTag:@"starts" rowType:XLFormRowDescriptorTypeDateTimeInline title:@"Starts"];
+        row.value = [NSDate dateWithTimeIntervalSinceNow:60*60*24];
+        [section addFormRow:row];
+
+         */
     }
 
 }
